a2de385a42751ba937e335df625619e427bca707
ukeyim <ukeyim@gmail.com>
2017-08-27 12:42:24 +0800

    init project

---
+++ index.ios.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// var React = require('react-native');
+// var Featured = require('./Featured');
+// var Search = require('./Search');
+
+// var {
+//     AppRegistry,
+//     TabBarIOS,
+//     Component
+//    } = React;
+
+import React, {Component} from 'react';
+import Featured from './Featured';
+import Search from './Search';
+import {
+    AppRegistry,
+    TabBarIOS,
+    Text
+} from 'react-native';
+
+export default class BookSearch extends Component {
+// class BookSearch extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'featured'
+        };
+    }
+
+    render() {
+        return (
+            <TabBarIOS selectedTab={this.state.selectedTab}>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'featured'}
+                    icon='featured'
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'featured'
+                        });
+                    }}>
+                    <Featured/>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'search'}
+                    icon='search'
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'search'
+                        });
+                    }}>
+                    <Search/>
+                </TabBarIOS.Item>
+            </TabBarIOS>
+        );
+    }
+}
+
+AppRegistry.registerComponent('BookSearch', () => BookSearch);
